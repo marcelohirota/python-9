5d93e3b03e02e3fd58b7b95c8fa6f93b4dfe64a0
Marcelo Almeida Hirota <marcelohirota@gmail.com>
2020-06-17 18:43:24 +0930

    Validation

--- datadesign/api/models.py
+++ datadesign/api/models.py
@@ -1,11 +1,23 @@
 from django.db import models
+from django.core.validators import EmailValidator, MinLengthValidator, validate_ipv4_address, ValidationError
+
+
+def level_validator(level):
+    if level not in ['CRITICAL', 'DEBUG', 'ERROR', 'WARNING', 'INFO']:
+        raise ValidationError(
+            'Level not allowed. Use CRITICAL, DEBUG, ERROR, WARNING or INFO')
 
 
 class User(models.Model):
     name = models.CharField('Nome', max_length=50)
     last_login = models.DateTimeField('Último login', auto_now=True)
-    email = models.CharField('E-mail', max_length=254)
-    password = models.CharField('Nome', max_length=50)
+    email = models.CharField(
+        'E-mail', max_length=254, validators=[EmailValidator()])
+    password = models.CharField(
+        'Nome', max_length=50, validators=[MinLengthValidator(8)])
+
+    def __str__(self):
+        return self.name
 
 
 class Agent(models.Model):
@@ -13,11 +25,16 @@ class Agent(models.Model):
     status = models.BooleanField('Status')
     env = models.CharField('Env', max_length=20)
     version = models.CharField('Version', max_length=5)
-    address = models.GenericIPAddressField('IP Address', max_length=39)
+    address = models.GenericIPAddressField(
+        'IP Address', max_length=39, validators=[validate_ipv4_address])
+
+    def __str__(self):
+        return self.name
 
 
 class Event(models.Model):
-    level = models.CharField('Level', max_length=20)
+    level = models.CharField(
+        'Level', max_length=20, validators=[level_validator])
     data = models.TextField('Data')
     arquivado = models.BooleanField('Arquivado')
     date = models.DateField('Date', auto_now=True)
@@ -27,6 +44,9 @@ class Event(models.Model):
 
 class Group(models.Model):
     name = models.CharField('Nome', max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class GroupUser(models.Model):
